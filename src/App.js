ec8aadd6eb2919ec272102c09c017a6cfda1765f
Dragan Danicic <dad1bg@bosch.com>
2021-10-22 15:44:01 +0200

    iinital commit, tailwind active

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {ButtonAtom, H1Atom} from './components';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <ButtonAtom/>
+        <H1Atom/>
       </header>
     </div>
   );
---
+++ src/components/button/ButtonAtom.js
@@ -0,0 +1,3 @@
+export default () => (
+    <button class=" hover:bg-white bg-green-700 text-3xl rounded-xl p-2"> Click Me </button>
+)
